073bf2ce9f03c480fe59da2988b2b3d479be468b
BahadirGecgel <bahadir.gecgel@gmail.com>
2022-01-12 23:50:56 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/bahadir-g/krakend-gin-logger
+replace github.com/ifaisalalam/krakend-gin-logger => github.com/bahadir-g/krakend-gin-logger v1.4.0
+
+go 1.15
+
+require (
+	github.com/devopsfaith/krakend-gologging v1.4.0
+	github.com/devopsfaith/krakend-logstash v1.4.0
+	github.com/gin-gonic/gin v1.7.2
+	github.com/luraproject/lura v1.4.1
+)
---
+++ logger.go
@@ -0,0 +1,108 @@
+// Copyright 2021 Faisal Alam. All rights reserved.
+// Use of this source code is governed by a Apache style
+// license that can be found in the LICENSE file.
+
+package gin_logger
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	gologging "github.com/devopsfaith/krakend-gologging"
+	logstash "github.com/devopsfaith/krakend-logstash"
+	"github.com/gin-gonic/gin"
+	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
+)
+
+const (
+	Namespace  = "github_com/bahadir-g/krakend-gin-logger"
+	moduleName = "krakend-gin-logger"
+)
+
+func NewLogger(cfg config.ExtraConfig, logger logging.Logger, loggerConfig gin.LoggerConfig) gin.HandlerFunc {
+	v, ok := ConfigGetter(cfg).(Config)
+	if !ok {
+		return gin.LoggerWithConfig(loggerConfig)
+	}
+
+	loggerConfig.SkipPaths = v.SkipPaths
+	logger.Info(fmt.Sprintf("%s: total skip paths set: %d", moduleName, len(v.SkipPaths)))
+
+	loggerConfig.Output = ioutil.Discard
+	loggerConfig.Formatter = Formatter{logger, v}.DefaultFormatter
+	return gin.LoggerWithConfig(loggerConfig)
+}
+
+type Formatter struct {
+	logger logging.Logger
+	config Config
+}
+
+func (f Formatter) DefaultFormatter(params gin.LogFormatterParams) string {
+	record := map[string]interface{}{
+		"method":             params.Method,
+		"host":               params.Request.Host,
+		"path":               params.Path,
+		"body":               params.Request.Body,
+		"header":             params.Request.Header,
+		"status_code":        params.StatusCode,
+		"user_agent":         params.Request.UserAgent(),
+		"client_ip":          params.ClientIP,
+		"latency":            params.Latency,
+		"response_timestamp": params.TimeStamp,
+	}
+
+	payload := map[string]interface{}{
+		"data": record,
+	}
+
+	if f.config.Logstash {
+		f.logger.Info("", payload)
+	} else {
+		p, _ := json.Marshal(payload)
+		f.logger.Info(string(p))
+	}
+
+	return ""
+}
+
+func ConfigGetter(e config.ExtraConfig) interface{} {
+	v, ok := e[Namespace]
+	if !ok {
+		return nil
+	}
+	tmp, ok := v.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	cfg := defaultConfigGetter()
+	if skipPaths, ok := tmp["skip_paths"].([]interface{}); ok {
+		var paths []string
+		for _, skipPath := range skipPaths {
+			if path, ok := skipPath.(string); ok {
+				paths = append(paths, path)
+			}
+		}
+		cfg.SkipPaths = paths
+	}
+	if v, ok = e[gologging.Namespace]; ok {
+		_, cfg.Logstash = e[logstash.Namespace]
+	}
+
+	return cfg
+}
+
+func defaultConfigGetter() Config {
+	return Config{
+		SkipPaths: []string{},
+		Logstash:  false,
+	}
+}
+
+type Config struct {
+	SkipPaths []string
+	Logstash  bool
+}
